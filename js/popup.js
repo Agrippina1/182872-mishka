8730f58d0f38028611783d3e8135daeadb010a3d
Larisa <grusha-si@yandex.ru>
2017-03-19 18:04:49 +0300

    Исправление ошибок

--- js/popup.js
+++ js/popup.js
@@ -15,26 +15,57 @@ navToggle.addEventListener('click', function() {
 
 
 var link = document.querySelector(".product__btn");
-var addBasket = document.querySelector(".catalog__basket");
+var linkSecond = document.querySelector(".catalog__basket");
 var popup = document.querySelector(".modal");
 var overlay = document.querySelector(".modal__overlay");
 var close = popup.querySelector(".modal__btn");
 
 link.addEventListener("click", function(event) {
   event.preventDefault();
-  popup.classList.add("modal__show");
-  overlay.classList.add("modal__overlay-show");
+  popup.classList.add("modal--show");
+  overlay.classList.add("modal--overlay-show");
+});
+linkSecond.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("modal--show");
+  overlay.classList.add("modal--overlay-show");
 });
 close.addEventListener("click", function(event) {
   event.preventDefault();
-  popup.classList.remove("modal__show");
-  overlay.classList.remove("modal__overlay-show");
+  popup.classList.remove("modal--show");
+  overlay.classList.remove("modal--overlay-show");
 });
 window.addEventListener("keydown", function(event) {
 if (event.keyCode === 27) {
-  if (popup.classList.contains("modal__show")) {
-    popup.classList.remove("modal__show");
-    overlay.classList.remove("modal__overlay-show");
+  if (popup.classList.contains("modal--show")) {
+    popup.classList.remove("modal--show");
+    overlay.classList.remove("modal--overlay-show");
   }
 }
 });
+
+
+
+// var addBasket = document.querySelector(".catalog__basket");
+// var popup = document.querySelector(".modal");
+// var overlay = document.querySelector(".modal__overlay");
+// var close = popup.querySelector(".modal__btn");
+//
+// link.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   popup.classList.add("modal__show");
+//   overlay.classList.add("modal__overlay-show");
+// });
+// close.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   popup.classList.remove("modal__show");
+//   overlay.classList.remove("modal__overlay-show");
+// });
+// window.addEventListener("keydown", function(event) {
+// if (event.keyCode === 27) {
+//   if (popup.classList.contains("modal__show")) {
+//     popup.classList.remove("modal__show");
+//     overlay.classList.remove("modal__overlay-show");
+//   }
+// }
+// });
